4a2f1ea769cbf25ae411942754656a1b37149bf4
EdMaccy <edwmaccy@hotmail.com>
2025-03-27 20:54:14 -0300

    ultimo commit

--- scripts.js
+++ scripts.js
@@ -1,6 +1,7 @@
 const convertButton = document.querySelector(".convert-button");
 const currencySelect = document.querySelector(".currency-select");
 
+
 function convertValues() {
     const inputCurrencyValue = document.querySelector(".input-currency").value;
     const currencyValueToConvert = document.querySelector(".currency-value-to-convert");
@@ -10,6 +11,13 @@ function convertValues() {
     const euroToday = 6.2;
     const libraToday = 7.5;
     const bitcoinToday = 87.4;
+
+    if (!inputCurrencyValue || isNaN(inputCurrencyValue)) {
+        alert("Por favor, insira um valor numérico válido.");
+        return; // Interrompe a execução caso a entrada seja inválida
+    }
+
+
 
     if (currencySelect.value == "dolar") {
         currencyValueConverted.innerHTML = new Intl.NumberFormat("en-US", {
@@ -44,6 +52,8 @@ function convertValues() {
         style: "currency",
         currency: "BRL",
     }).format(inputCurrencyValue);
+
+
 }
 
 function changeCurrency(){
@@ -71,10 +81,11 @@ function changeCurrency(){
     }
 
     convertValues()
+
+    
+    
 }
         
-
-
-
 currencySelect.addEventListener("change", changeCurrency);
 convertButton.addEventListener("click", convertValues);
+
